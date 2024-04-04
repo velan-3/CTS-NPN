99fecf32cf26050f94c7fe7f106db88621280a83
Velan <velan2003chandru@gmail.com>
2024-04-04 16:03:55 +0530

    full code

--- guessnumber.py
+++ guessnumber.py
@@ -1,1 +1,15 @@
-print("hello world")
+import numpy as np # type: ignore
+
+def numbername():
+    
+    while True:
+        num = np.random(1,100)
+        user_num = input("Enter the number you have guessed..")
+        
+        if(num==user_num):
+            print("Absolutely!You are correct")
+        elif(num>user_num):
+            print("It's too high from the correct answer")
+        else:
+            print("It's too low from the correct answer")   
+        
